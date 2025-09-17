c5054ddeb202cd71553f50bd9afca378cdceff80
Arvind Bishnoi <arvind.bishnoi@indiamart.com>
2025-09-17 23:21:29 +0530

    test

--- backend/modules/tripPlanner/controller/findDestination/findDestinationController.go
+++ backend/modules/tripPlanner/controller/findDestination/findDestinationController.go
@@ -1,7 +1,12 @@
 package findDestinationController
 
-import "github.com/gin-gonic/gin"
+import (
+	globalFuctions "trip-planner-backend/utilities/globalFunctions"
+
+	"github.com/gin-gonic/gin"
+)
 
 func FindDestination(ginCtx *gin.Context) {
+	globalFuctions.WirteJsonLogs(ginCtx, "arvind_testing", map[string]interface{}{"arvind": "k"})
 	ginCtx.Status(200)
 }
