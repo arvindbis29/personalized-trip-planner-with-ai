32463a770cec496459eebfaf92a6e9089477249d
AnkurMPansara <ankur.at.surat@gmail.com>
2025-09-19 12:50:31 +0530

    added api logic

--- trip-planner-backend/modules/tripPlanner/controller/findDestination/findDestinationController.go
+++ trip-planner-backend/modules/tripPlanner/controller/findDestination/findDestinationController.go
@@ -1,12 +1,34 @@
 package findDestinationController
 
 import (
+	"net/http"
+	findDestinationModel "trip-planner-backend/modules/tripPlanner/model/findDestination"
 	"trip-planner-backend/utilities/globalFunctions"
 
 	"github.com/gin-gonic/gin"
 )
 
 func FindDestination(ginCtx *gin.Context) {
+	apiInputParam, bindErr := BindInputParams(ginCtx)
+	apiResponse := findDestinationModel.ApiResponse{}
+	if bindErr != nil {
+		apiResponse.Code= http.StatusBadRequest
+		apiResponse.Status = "Failure"
+		apiResponse.Response = map[string]any{
+			"reponse" : nil,
+			"error" : bindErr.Error(),
+		}
+		ReturnApiResponse(ginCtx, http.StatusBadRequest, apiResponse)
+	}
 	globalFunctions.WriteJsonLogs(ginCtx, "arvind_testing", map[string]any{"arvind": "k"})
 	ginCtx.Status(200)
+}
+
+func BindInputParams(ginCtx *gin.Context) (InputParams findDestinationModel.BindingInputParams, err error) {
+	bindErr := ginCtx.ShouldBindBodyWithJSON(&InputParams)
+	return InputParams, bindErr
+}
+
+func ReturnApiResponse(ginCtx *gin.Context, apiCode int, apiResponse findDestinationModel.ApiResponse) {
+	ginCtx.JSON(apiCode, apiResponse)
 }
